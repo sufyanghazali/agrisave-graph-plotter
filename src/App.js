f0be2e2c8aee23b3a1a31650c38749e0ca00613c
Sufyan Ghazali <sufyan.ghazali@gmail.com>
2021-08-30 14:57:18 +0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
+import { queryIotCatalogsBySerialNumberIndex } from './graphql/queries.js';
 
-function App() {
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
+const App = () => {
+  const [data, setData] = useState([]);
+
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    try {
+      const sensorData = await API.graphql(graphqlOperation(queryIotCatalogsBySerialNumberIndex));
+      console.log(sensorData.data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
 export default App;
