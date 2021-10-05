893624d1c01006a78ff268fb495e888050ec9c4c
Sufyan Ghazali <sufyan.ghazali@gmail.com>
2021-10-05 19:01:39 +0800

    readded graphql api

---
+++ src/graphql/mutations.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createIotCatalogs = /* GraphQL */ `
+  mutation CreateIotCatalogs($input: CreateIotCatalogsInput!) {
+    createIotCatalogs(input: $input) {
+      deviceLat
+      deviceLon
+      deviceMos
+      deviceTemp
+      serialNumber
+      unixTimeStamp
+    }
+  }
+`;
+export const deleteIotCatalogs = /* GraphQL */ `
+  mutation DeleteIotCatalogs($input: DeleteIotCatalogsInput!) {
+    deleteIotCatalogs(input: $input) {
+      deviceLat
+      deviceLon
+      deviceMos
+      deviceTemp
+      serialNumber
+      unixTimeStamp
+    }
+  }
+`;
+export const updateIotCatalogs = /* GraphQL */ `
+  mutation UpdateIotCatalogs($input: UpdateIotCatalogsInput!) {
+    updateIotCatalogs(input: $input) {
+      deviceLat
+      deviceLon
+      deviceMos
+      deviceTemp
+      serialNumber
+      unixTimeStamp
+    }
+  }
+`;
